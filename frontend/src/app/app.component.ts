812d9087a609a994c412a6edf05190615deb6041
Eranga Hansaja <erangahansaja222@gmail.com>
2025-09-11 23:59:02 +0530

    create folder structure

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
-  template: `<h1>Task Manager App</h1><p>API URL: {{ apiUrl }}</p>`,
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  apiUrl = environment.apiUrl;
+  title = 'frontend';
 }
