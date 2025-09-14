e3b9d88eb430c7c6546f5dea8a66fc25463c6f85
Eranga Hansaja <erangahansaja222@gmail.com>
2025-09-15 00:15:39 +0530

    set up basic routing for login, register, and dashboard

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// Components and guard will be imported here in later steps
+
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' }, // Default to login
+  { path: 'login', /* component: LoginComponent */ }, // Uncomment and add import later
+  { path: 'register', /* component: RegisterComponent */ }, // Uncomment and add import later
+  { path: 'dashboard', /* component: DashboardComponent, canActivate: [AuthGuard] */ }, // Uncomment and add imports later
+  { path: '**', redirectTo: '/login' } // Wildcard to login
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
