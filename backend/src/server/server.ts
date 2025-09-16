a77cf659c194d38ef1d308bd31b088f8cac1e2fb
Eranga Hansaja <erangahansaja222@gmail.com>
2025-09-17 02:26:40 +0530

    Added CORS middleware to allow frontend requests from localhost

--- backend/src/server/server.ts
+++ backend/src/server/server.ts
@@ -1,12 +1,14 @@
 import dotenv from 'dotenv';
 import path from 'path';
 import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
 import routers from '../routers';
 
 dotenv.config({ path: path.resolve(__dirname, '../../env/.env') });
 
 const app: Application = express();
 
+app.use(cors({ origin: 'http://localhost:4200' }));
 app.use(express.json());
 
 routers.forEach((router) => app.use('/api', router));
